44e139d5b71ecd462fedcd71c3e88d8152a132c7
Chioukhhmimi <thekinghc1996@gmail.com>
2026-09-05 13:55:42 +0100

    fix: add react-native-reanimated/plugin to babel config

--- babel.config.js
+++ babel.config.js
@@ -21,6 +21,7 @@ module.exports = function (api) {
           extensions: ['.ios.js', '.android.js', '.js', '.jsx', '.ts', '.tsx', '.json'],
         },
       ],
+      'react-native-reanimated/plugin',
     ],
   };
 };
